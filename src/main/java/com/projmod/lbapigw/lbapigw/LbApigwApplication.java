70dace1650dc2955d02353b47e9d5d3e63f43363
Swaraj <swaraj@nair.com>
2025-12-25 18:57:50 +0530

    initial commit

---
+++ src/main/java/com/projmod/lbapigw/lbapigw/LbApigwApplication.java
@@ -0,0 +1,13 @@
+package com.projmod.lbapigw.lbapigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbApigwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbApigwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projmod/lbapigw/lbapigw/LbApigwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projmod.lbapigw.lbapigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbApigwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
